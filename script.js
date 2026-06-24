451224bd657ef482acdbd9901d3845c33fe7d8ef
James Brown <83186482+jbrown-js@users.noreply.github.com>
2026-06-24 03:38:18 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function generatePassword() {
+    var uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
+    var numberChars = '0123456789';
+    var symbolChars = '!@#$%^&*()';
+    var allChars = uppercaseChars + lowercaseChars + numberChars + symbolChars;
+    var password = ''; 
+
+    for (var i = 0; i < 18; i++) {
+        var randomIndex = Math.floor(Math.random() * allChars.length);
+        password += allChars[randomIndex]; // Add random character to password
+    }
+
+    document.getElementById('password').value = password;
+}
